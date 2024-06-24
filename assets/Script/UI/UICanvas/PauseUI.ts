1a20d080395aaa8a3c6fac6dbf3e056f199abaf9
Nguyenanh393 <93720800+Nguyenanh393@users.noreply.github.com>
2024-06-24 15:20:05 +0700

    Responsive

--- assets/Script/UI/UICanvas/PauseUI.ts
+++ assets/Script/UI/UICanvas/PauseUI.ts
@@ -3,6 +3,8 @@ import UICanvas from '../UICanvas';
 import { BlockController } from '../../Manager/BlockController';
 import { GamePlay } from './GamePlay';
 import { UIManager } from '../UIManager';
+import { CharacterManager } from '../../Manager/CharacterManager';
+import { LevelManager } from '../../Manager/LevelManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('PauseUI')
@@ -10,6 +12,14 @@ export class PauseUI extends UICanvas {
     
     onResumeButtonClicked() {
         BlockController.getInstance().turnOnNodeBlockEvent();
+        UIManager.getInstance().openUI(GamePlay);
+        this.close(0);
+    }
+
+    onRestartButtonClicked() {
+        BlockController.getInstance().reset();
+        CharacterManager.getInstance().reset();
+        LevelManager.getInstance().loadCurrentLevel();
         UIManager.getInstance().openUI(GamePlay);
         this.close(0);
     }
--- assets/Script/UI/UICanvas/WinUI.ts
+++ assets/Script/UI/UICanvas/WinUI.ts
@@ -1,12 +1,24 @@
 import { _decorator, Component, Node } from 'cc';
 import UICanvas from '../UICanvas';
 import { BlockController } from '../../Manager/BlockController';
+import { LevelManager } from '../../Manager/LevelManager';
+import { CharacterManager } from '../../Manager/CharacterManager';
+import { UIManager } from '../UIManager';
+import { GamePlay } from './GamePlay';
 const { ccclass, property } = _decorator;
 
 @ccclass('WinUI')
 export class WinUI extends UICanvas {
     onResumeButtonClicked() {
         BlockController.getInstance().turnOnNodeBlockEvent();
+        this.close(0);
+    }
+
+    onRestartButtonClicked() {
+        BlockController.getInstance().reset();
+        CharacterManager.getInstance().reset();
+        LevelManager.getInstance().loadCurrentLevel();
+        UIManager.getInstance().openUI(GamePlay);
         this.close(0);
     }
 }
